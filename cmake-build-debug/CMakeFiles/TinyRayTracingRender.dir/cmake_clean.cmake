c96f171504ad63252654b3e02c8a59d7f055c418
m-iDev-0792 <1015732477@qq.com>
2018-05-25 14:52:56 +0800

    init version

---
+++ cmake-build-debug/CMakeFiles/TinyRayTracingRender.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TinyRayTracingRender.dir/main.cpp.o"
+  "TinyRayTracingRender.pdb"
+  "TinyRayTracingRender"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TinyRayTracingRender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
